79f1541f89919a34e273ca69272a88a7496e453c
aravinthanscott <aravinthan17121996@gmail.com>
2019-12-16 11:37:31 +0530

    I have done

---
+++ src/Student.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Student {
+public static void main(String[] args) {
+	
+	System.out.println("Enter Student Id");
+	Scanner sc= new Scanner(System.in);
+	int Id = sc.nextInt();
+	System.out.println("The Student ID Is " +Id);
+	System.out.println("Enter The Name");
+	String name = sc.next();
+	System.out.println("The Student Name Is "+name);
+	System.out.println("Enter The Email ID");
+    String email =sc.next();
+	System.out.println("THE MAIL ID IS"+email);
+	System.out.println("Enter The PhoneNo");
+	long phone = sc.nextLong();
+	System.out.println("The Phone No Is" +phone);
+	System.out.println("Enter The Dept");
+	String dept= sc.next();
+	System.out.println("THE DEPARTMENT IS  "+dept);
+	System.out.println("Enter The Gender");
+	String gender= sc.next();
+	System.out.println("The Gender Is " +gender);
+	System.out.println("Enter The City");
+	String city= sc.next();
+	System.out.println("The City Is "+city);
+
+
+}
+}
